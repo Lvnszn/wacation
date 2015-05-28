161b2a1c6a0a77eb4e28e19019d589c4b9462b0d
Lvnszn <jason.peng@xplusz.com>
2015-05-28 15:01:27 +0800

    first

---
+++ src/main/java/com/test/Demo.java
@@ -0,0 +1,21 @@
+package com.test;
+
+/**
+ * Created by jasonpeng on 5/27/15.
+ */
+public class Demo extends javax.servlet.http.HttpServlet {
+        protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+                    String username = request.getParameter("username");
+                    String department = request.getParameter("department");
+                    String type = request.getParameter("type");
+                    String start = request.getParameter("start");
+                    String end = request.getParameter("end");
+
+                    System.out.println(username+department+type+start+end);
+
+                }
+
+        protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+                    doPost(request,response);
+                }
+        }
